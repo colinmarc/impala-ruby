2fec4ed14dd7d39301ec6f8b05e334a13aa19eb0
Colin Marc <colinmarc@gmail.com>
2013-01-23 02:06:50 -0800

    fix loading for generated files

--- lib/impala.rb
+++ lib/impala.rb
@@ -1,3 +1,7 @@
+this_dir = File.expand_path(File.dirname(__FILE__))
+gen_dir = File.join(this_dir, 'impala', 'protocol')
+$LOAD_PATH.unshift(gen_dir) unless $LOAD_PATH.include?(gen_dir)
+
 require 'impala/version'
 require 'impala/protocol'
 
